0fa208ae44e77b36c951bafb46cee3c56e184ef4
ZEN <zen@ctrlworks.com>
2016-06-01 20:47:35 +0800

    refactor to simplified design, add comment to describe how it's work

--- overlap.py
+++ overlap.py
@@ -1,58 +1,83 @@
 import datetime
 
-class DateOverlap(object):
-    """docstring for DateOverlap"""
-    def __init__(self):
-        self._holidays = {}
+def conv_date_string(date_strs):
+    """
+    Data structure (Dict):
+    ===
+    Key=NumOfWeek, Val=code
+    code = 1 byte = int( X|Sat|Fri|Thu|Wed|Tue|Mon|Sun )
 
-    def _conv_data_structure(self, date_str):
+
+    Example:
+    ===
+    holiday_list = ["2016-05-01(SUN)", "2016-05-02(MON)", "2016-05-03(TUE)"]
+    num_of_week, code = conv_date_string(holiday_list)
+    num_of_week, code = 18, 7(0000 0111b)
+
+    if we build the table base on years:
+    holidays = {
+         1: 0, # 0000 0000b
+        ...
+        18: 7,   # 0000 0111b
+        19: 127, # 0111 1111b
+        ...
+        52: 0, # 0000 0000b
+    }
+
+    dates_for_check = ["2016-05-03(TUE)", "2016-05-04(WED)", "2016-05-05(THU)"]
+    num_of_week, code = conv_date_string(dates_for_check)
+    num_of_week, code = 18, 28(0001 1100b)
+
+    Compare:
+    holidays[18] = 0000 0111b
+    query_code   = 0001 1100b (AND)
+    ===============================
+                   0000 0100b
+
+    ANS: weeks 18, Tuedays conflict.
+
+    so, 52 bytes should be cover whole years holidays.
+    code range from 0 ~ 127 (only 7 bit)
+    """
+
+    date_code = {}
+    for date_str in date_strs:
         date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d")
         num_of_week = date_obj.isocalendar()[1]
         weekday = date_obj.weekday()
-        return (num_of_week, weekday)
 
-    def add(self, date_str):
-        (num_of_week, weekday) = self._conv_data_structure(date_str)
-        if num_of_week not in self._holidays:
-            self._holidays[num_of_week] = 0
-        self._holidays[num_of_week] = self._holidays[num_of_week] | 1 << weekday
-        print "add date: %s, {%s: %s - %s}" % (date_str, num_of_week, weekday, self._holidays[num_of_week])
+        if num_of_week not in date_code:
+            date_code[num_of_week] = 0
+        date_code[num_of_week] = date_code[num_of_week] | 1 << weekday
+    return date_code
 
-    def remove(self, date_str):
-        pass
+def overlap_report(holidays, query_dates):
+    for num_of_week, query_code in query_dates.items():
+        overlap = query_code & holidays.get(num_of_week, 0)
+        if overlap > 0:
+            print ("overlap detected: Number of week: %s, conflict code: %s(%s)"
+                   % (num_of_week, bin(overlap), overlap))
 
-    def is_overlap(self, date_str_list=[]):
-        # generate compare list
-        temp = {}
-        for date_str in date_str_list:
-            (num_of_week, weekday) = self._conv_data_structure(date_str)
-            if num_of_week not in temp:
-                temp[num_of_week] = 0
-            temp[num_of_week] = temp[num_of_week] | 1 << weekday
-
-        for key, val in temp.items():
-            result = self._holidays[key] | val
-            if result > 0:
-                print "W: %s, BIN: %s" % (key, bin(result))
 
 if __name__ == '__main__':
-    overlap = DateOverlap()
-    overlap.add("2016-04-30")
-    overlap.add("2016-05-01")
-    overlap.add("2016-05-02")
-    overlap.add("2016-05-03")
-    overlap.add("2016-05-04")
-    overlap.add("2016-05-05")
-    overlap.add("2016-05-06")
-    overlap.add("2016-05-07")
-    overlap.add("2016-05-08")
-    overlap.add("2016-05-09")
-    overlap.add("2016-05-10")
+    holidays_list = ["2016-04-30",
+                     "2016-05-01",
+                     "2016-05-02",
+                     "2016-05-03",
+                     "2016-05-04",
+                     "2016-05-05",
+                     "2016-05-06",
+                     "2016-05-07",
+                     "2016-05-08",
+                     "2016-05-09",
+                     "2016-05-10"]
 
-    print "is overlap", overlap.is_overlap(["2016-05-05",
-                                            "2016-05-06",
-                                            "2016-05-07",
-                                            "2016-05-08",
-                                            "2016-05-09",
-                                            "2016-05-10",
-                                            "2016-05-01",])
+    holidays = conv_date_string(holidays_list)
+
+    dates_for_check = ["2016-05-02",
+                       "2016-05-03",
+                       "2016-05-04",
+                       "2016-05-21"]
+    query_dates = conv_date_string(dates_for_check)
+
+    overlap_report(holidays, query_dates)
